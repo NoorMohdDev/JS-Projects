db087969d5d9b9c0edb3eccd3f1eab12d17af795
NoorMohdDev <noormohddev@gmail.com>
2025-07-13 19:32:33 +0530

    add rgbColor generator, css

--- Random Colour Generator/script.js
+++ Random Colour Generator/script.js
@@ -1,12 +1,15 @@
 const hexBtn = document.querySelector(".hex-btn");
 const hexCopyColor = document.querySelector(".hex-copy-color");
 let hexColorValue = document.querySelector(".hex-color-value");
-let hexColorContainer = document.querySelector(
-  ".hex-color-container"
-);
+let hexColorContainer = document.querySelector(".hex-color-container");
 
 const rgbBtn = document.querySelector(".rgb-btn");
 const rgbCopyColor = document.querySelector(".rgb-copy-color");
+let rgbColorValue = document.querySelector(".rgb-color-value");
+let rgbColorContainer = document.querySelector(".rgb-color-container");
+let green = 0;
+let red = 0;
+let blue = 0;
 
 hexBtn.addEventListener("click", () => {
   const str = "abcdef0123456789";
@@ -17,8 +20,40 @@ hexBtn.addEventListener("click", () => {
   }
 
   hexColorValue.innerHTML = `#${newHexValue}`;
-  
-  hexColorContainer.style.backgroundColor = hexColorValue.innerHTML;    
+
+  hexColorContainer.style.backgroundColor = hexColorValue.innerHTML;
 });
 
-const createRGBColor = () => {};
+rgbBtn.addEventListener("click", () => {
+  red = Math.floor(Math.random() * 255);
+  gren = Math.floor(Math.random() * 255);
+  blue = Math.floor(Math.random() * 255);
+
+  document.querySelectorAll("input").forEach((element) => {
+    if (element.id === "red") {
+        element.value = red;
+      } else if (element.id === "blue") {
+        element.value = blue;
+      } else {
+        element.value = green;
+      }
+  });
+
+  rgbColorValue.innerHTML = `rgb(${red},${green},${blue})`;
+
+  rgbColorContainer.style.backgroundColor = rgbColorValue.innerHTML;
+});
+
+document.querySelectorAll("input").forEach((element) => {
+  element.addEventListener("change", () => {
+    if (element.id === "red") {
+      red = element.value;
+    } else if (element.id === "blue") {
+      blue = element.value;
+    } else {
+      green = element.value;
+    }
+    rgbColorValue.innerHTML = `rgb(${red},${green},${blue})`;
+    rgbColorContainer.style.backgroundColor = rgbColorValue.innerHTML;
+  });
+});
