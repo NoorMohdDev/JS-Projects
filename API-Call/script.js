d1c0866a0e869c7e2d64c6e9833fc57459fba489
NoorMohdDev <noormohddev@gmail.com>
2025-07-15 10:19:41 +0530

    add API call using xhr, promise with fetch , async/await with fetch

--- API-Call/script.js
+++ API-Call/script.js
@@ -2,14 +2,17 @@ const postListContainer = document.querySelector(".posts-list-container");
 const apiURL = "https://jsonplaceholder.typicode.com/posts";
 
 const showData = (apiResponse) => {
-  postListContainer.innerHTML = apiResponse.map(({title,body}) =>
-    `
-    <div>
+  postListContainer.innerHTML = apiResponse
+    .map(
+      ({ title, body }) =>
+        `
+    <div class="post-item">
         <h1>${title}</h1>
         <p>${body}</p>
     </div>
     `
-  ).join(" ");
+    )
+    .join(" ");
 };
 
 const xhrApiCall = () => {
@@ -26,6 +29,54 @@ const xhrApiCall = () => {
     }
   };
 };
-xhrApiCall();
 
-// showData();
+// xhrApiCall();
+
+const fetchApiCall = () => {
+  const fetchResponse = fetch(apiURL, {
+    method: "GET",
+  });
+
+  fetchResponse
+    .then((res) => res.json())
+    .then((res) => showData(res))
+    .catch((e) => console.log(e));
+};
+
+// fetchApiCall();
+
+const fetchAsyncAwait = async () => {
+  const fetchResponse = await fetch(apiURL, {
+    method: "GET",
+  });
+
+  const res = await fetchResponse.json();
+  showData(res);
+};
+
+// fetchAsyncAwait();
+
+const auxilaryMethod = (method, API) => {
+  return new Promise((res, rej) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open(method, API);
+    xhr.responseType = "json";
+    xhr.send();
+    xhr.onload = () => {
+      if (xhr.status === 200) {
+        res(xhr.response);
+        //   console.log("APi fetched successfully", xhr.response);
+      } else {
+        rej(xhr.response)
+      }
+    };
+  });
+};
+
+const fetchAsyncAwaitAndXHR = async () => {
+
+  await auxilaryMethod("GET", apiURL)
+
+};
+
+fetchAsyncAwaitAndXHR()
