380efe92283fa03f138bca1ea58fda02e89218c1
NoorMohdDev <noormohddev@gmail.com>
2025-07-19 12:37:17 +0530

    add persitent toggle

--- Light-Dark Mode/script.js
+++ Light-Dark Mode/script.js
@@ -2,18 +2,33 @@ const toggleTheme = document.querySelector(".toggle-theme");
 const body = document.querySelector("body");
 const toggleHeading = document.querySelector(".toggle-heading");
 
-const elements = [body,toggleHeading,toggleTheme]
+const elements = [body, toggleHeading, toggleTheme];
+
+document.onload = defaultTheme();
 
 toggleTheme.addEventListener("click", () => {
   changeTheme();
 });
 
-const changeTheme = () => {
-    elements.forEach(item =>{
-        if (item.classList.contains("dark")) {
-            item.classList.remove("dark")
-        }
-        else{
-            item.classList.add("dark")
-        }
-})};
+function defaultTheme() {
+  const isDarkActive = localStorage.getItem("dark");
+//   console.log(isDarkActive);
+  
+  if (isDarkActive) {
+    changeTheme();
+  }
+}
+
+function changeTheme(){
+  elements.forEach((item) => {
+    if (item.classList.contains("dark")) {
+      item.classList.remove("dark");
+    } else {
+      item.classList.add("dark");
+    }
+  });
+
+  if (elements.some((item) => item.classList.contains("dark"))) {
+    localStorage.setItem("dark", true);
+  } else localStorage.setItem("dark",false);
+};
