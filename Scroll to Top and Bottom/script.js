907f29450ce66e69f6a2c0083d2784d5194986e0
NoorMohdDev <noormohddev@gmail.com>
2025-07-23 20:09:14 +0530

    Add initial files

---
+++ Scroll to Top and Bottom/script.js
@@ -0,0 +1,7 @@
+const scrollBottom = document.querySelector(".scroll-bottom");
+const scrollTop = document.querySelector(".scroll-top");
+const loader = document.querySelector(".loader");
+const userContainer = document.querySelector(".users-list");
+
+loadUsers();
+
