a11acda5605f97355a9da8595efc09dbed36b2e2
NoorMohdDev <noormohddev@gmail.com>
2025-07-23 20:10:47 +0530

    add smooth scrolling functionality

--- Scroll to Top and Bottom/script.js
+++ Scroll to Top and Bottom/script.js
@@ -5,3 +5,40 @@ const userContainer = document.querySelector(".users-list");
 
 loadUsers();
 
+scrollBottom.addEventListener("click", () => {
+  window.scrollTo({
+    top: document.body.scrollHeight,
+    behavior: "smooth",
+  });
+});
+
+scrollTop.addEventListener("click", () => {
+  window.scrollTo({
+    top: -document.body.scrollHeight,
+    behavior: "smooth",
+  });
+});
+
+async function loadUsers() {
+  loader.classList.add("show");
+
+  try {
+    let response = await fetch("https://dummyjson.com/users?limit=100", {
+      method: "GET",
+    });
+
+    response = await response?.json();
+    console.log(response);
+
+    userContainer.innerHTML = response.users
+      ?.map(
+        (user) =>
+          `<h2>${user.firstName} ${user.maidenName} ${user.lastName}</h2>`
+      )
+      .join(" ");
+    userContainer.classList.remove("hide");
+    loader.classList.remove("show");
+  } catch (error) {
+    console.log(error);
+  }
+}
