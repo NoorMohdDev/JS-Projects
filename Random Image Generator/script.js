4a9684fd7824cc61f40636600ab91a2a5d55c214
NoorMohdDev <noormohddev@gmail.com>
2025-07-27 13:20:14 +0530

    refactor: padding in styles

--- Random Image Generator/script.js
+++ Random Image Generator/script.js
@@ -2,8 +2,12 @@ const container = document.querySelector(".container");
 const loadMoreBtn = document.querySelector(".load-more");
 let count = 1;
 
+loadMoreBtn.addEventListener("click", () => {
+  fetchRandomImages((count += 5));
+});
+
 function fetchRandomImages(getCount) {
-  for (let i = getCount; i <= getCount + 7; i++) {
+  for (let i = getCount; i <= getCount + 3; i++) {
     let createElement = document.createElement("img");
     createElement.src = `https://picsum.photos/300?random=${i}`;
     container.appendChild(createElement);
@@ -11,7 +15,3 @@ function fetchRandomImages(getCount) {
 }
 
 fetchRandomImages(count);
-
-loadMoreBtn.addEventListener("click", () => {
-  fetchRandomImages((count += 5));
-});
